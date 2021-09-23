27245c851b591ad8947c4e8711f85dd04668d3c7
WangY <WangY0o@outlook.com>
2021-09-23 17:22:37 +0800

    setup 细节问题

--- TypeScrip/04_vueStudy/my-project/src/App.vue
+++ TypeScrip/04_vueStudy/my-project/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-    <h2>Child 子集组件</h2>
+    <h2>App 父集组件</h2>
+    <h3>msg:{{msg}}</h3>
+    <button @click="msg+='==='">更新数据</button>
+    <hr/>
+    <Child :msg="msg" />
 </template>
 
 <script lang="ts">
 
 import { defineComponent,reactive,ref } from 'vue';
+import Child from './components/Child.vue';
+
 
 //暴漏出去一个定义好的组件
 export default defineComponent({
-  name: 'Child',
-
+  name: 'App',
+  components: { Child },
+ 
+  setup(){
+    // 定义一个ref
+    const msg = ref('hello world')
+    return{
+      msg,
+    }
+  }
 });
 </script>
--- TypeScrip/04_vueStudy/my-project/src/components/Child.vue
+++ TypeScrip/04_vueStudy/my-project/src/components/Child.vue
@@ -1,12 +1,32 @@
 <template>  
     <h2>Child 子集组件</h2>
+    <h3>msg:{{msg}}</h3>
 </template>
 
 <script lang="ts" >
+
 import {defineComponent} from 'vue'
-export default {
-    name: 'Child'
-}
+
+
+export default defineComponent({
+    name: 'Child',
+    props:['msg'],
+    //setup 细节问题
+    // setup 是在beforeCreate生命周期回调之前就执行了，而且就执行一次
+    // 由此可以推断出：setup在执行的时候，当前的组件还没有创建出来，也就意味着组件实例对象根本就不能用
+    // this 是 undefined，说明，就不能通过this再去调用data/computed/methods/props中的相关内容了
+    // 其实所有的composition API相关回调函数中也都不可以
+    beforeCreate(){
+        console.log('beforeCreate执行了')
+    },
+    // mounted(){},
+    setup(){
+        console.log('setup执行了',this)
+        return {
+            // setup 中一般都是返回一个对象，对象中的属性和方法都可以在HTML模板中直接使用
+        }
+    }
+})
 </script>
 
 <style>
